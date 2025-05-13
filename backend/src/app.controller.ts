a01841e524ccab5afde87f3c3556bad35a52da03
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2025-05-13 17:41:11 +0200

    new api/front handler

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,17 +1,6 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
-import { join } from 'path';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   constructor() {}
-
-  @Get('*')
-  serveClient(@Req() req: any, @Res() res: any) {
-    if (req.originalUrl.startsWith('/api/')) {
-      res.status(404).json({ message: 'API route not found' });
-      return;
-    }
-
-    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
-  }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
@@ -11,9 +14,13 @@ import { GameModule } from './game/game.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api*'],
+    }),
+    GameModule,
     AuthModule,
     PrismaModule,
-    GameModule,
   ],
   controllers: [AppController],
   providers: [UsersService],
